13b1cf225a20bd20b1c9445583721af39becf95d
KevinHayes <khayes@naperville203.org>
2018-09-06 11:46:46 -0500

    added bluej project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #The First Test of this stuff
 This is a learning process of git
+This is another line of code
---
+++ again another Test/Program1.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Program1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Program1
+{
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public static void main (String [] args)
+    {
+        System.out.print("Hello Git");
+    }
+}
